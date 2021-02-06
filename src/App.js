302013ea7f7a647455e89f683fc1ed865a0a3a45
Mako3 <big.mako@gmail.com>
2021-02-05 23:40:13 -0800

    initialized pages, services is working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import Header from '../src/components/header/header.component';
-import Hero from '../src/components/hero/hero.component';
-import Cards from '../src/components/cards/cards.component';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
-import Footer from '../src/components/footer/footer';
-
-
-const useStyles = makeStyles((theme) => ({
-  '@global': {
-    ul: {
-      margin: 0,
-      padding: 2,
-      listStyle: 'none',
-    },
-  },
-}));
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Services from './pages/services';
 
 export default function App() {
-  const classes = useStyles();
 
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Header />
-      <Hero />
-      <Cards />
-      <Footer />
+      <Router>
+        <Switch>
+        <Route path='/' component={Services} />
+        {/* <Route path='/services' component={Services} />
+        <Route path='/brows' component={Brows} />
+        <Route path='/location' component={MyLocation} /> */}
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 }
---
+++ src/pages/services.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../components/header/header.component';
+import Hero from '../components/hero/hero.component';
+import Cards from '../components/cards/cards.component';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import Footer from '../components/footer/footer';
+
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 2,
+      listStyle: 'none',
+    },
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Header />
+      <Hero />
+      <Cards />
+      <Footer />
+    </React.Fragment>
+  );
+}
